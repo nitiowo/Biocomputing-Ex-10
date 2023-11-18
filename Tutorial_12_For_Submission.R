b8a69135b306dd858da6ec635dad2e3c5fdfe826
Grace Waddell <gwaddel3@nd.edu>
2023-11-17 19:55:03 -0500

    Update 3

--- Tutorial_12_For_Submission.R
+++ Tutorial_12_For_Submission.R
@@ -3,7 +3,12 @@
 #Exercise 1
 #Import Data
 scores <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-10/UWvMSU_1-22-13.txt")
-#For loop skeleton
+#Generate dataframe for the cumulative scores
+time <- c(0)
+UWScore <- c(0)
+MSUScore <- c(0)
+cumulativeScores <- data.frame(time,UWScore,MSUScore)
+#For loop
 for(i in 2:nrow(scores)){
   if(scores$V2[i]=="UW"){
     print(scores$V3[i])
