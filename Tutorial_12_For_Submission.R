0fbe689c304d7ff7a899e5f028e866a1778f24f6
Grace Waddell <gwaddel3@nd.edu>
2023-11-17 19:45:38 -0500

    Update 2

--- Tutorial_12_For_Submission.R
+++ Tutorial_12_For_Submission.R
@@ -1,5 +1,16 @@
 #Tutorial 12 Submission
 
 #Exercise 1
+#Import Data
+scores <- read.table("C:/Users/grace/Desktop/Biocomputing 2023/R/Tutorials/Biocomputing-Ex-10/UWvMSU_1-22-13.txt")
+#For loop skeleton
+for(i in 2:nrow(scores)){
+  if(scores$V2[i]=="UW"){
+    print(scores$V3[i])
+  }
+  else{
+    print(scores$V3[i])
+  }
+}
 
 #Exercise 2
