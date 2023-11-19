7d52770bda56ef5185a34f43e6cf9fc3f5b09e1d
mkrause3 <mkrause3@nd.edu>
2023-11-19 16:11:20 -0500

    pseudocode for part 2

---
+++ exercise10_script.R
@@ -0,0 +1,12 @@
+#### part 2 ----
+
+# have computer generate a number
+sample(1:100, 1)
+# ask for user-input number
+# compare the two using if else
+# if computer > user
+# print higher
+# if computer < user
+# print lower
+# if computer = user
+# print correct!
