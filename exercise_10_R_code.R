752cb793b240a837b52976ad3ba75987c78e8f1a
James Magas <jmagas@nd.edu>
2023-11-27 20:51:32 -0500

    Worked on plot

--- exercise_10_R_code.R
+++ exercise_10_R_code.R
@@ -16,7 +16,7 @@ library(cowplot)
 #import data and read the .txt file to a data frame
 
 data=read.table(file="UWvMSU_1-22-13.txt",header=TRUE)
-data
+head(data)
 
 #create a vector of the team column and call it team
 team<-c(data$team)
@@ -58,5 +58,23 @@ scores
 
 
 #for graph time is going to be x axis
+#I will need to create a line of scores vs time for MSU and another for UW
+#remove x tick marks
+#make graph start at 0 in x-axis and y-axis
 
 
+ggplot(scores, aes(x = Time)) +
+     geom_line(aes(y = MSU), size = 2, color = "darkgreen") +
+     geom_line(aes(y = UW), size = 2, color = "purple") +
+     xlab("1st") +
+   
+     ylab("Score") +
+     scale_x_continuous(expand = c(0,0), limits = c(0,NA)) +
+     scale_y_continuous(expand = c(0,0), limits = c(0,NA)) +
+     
+     theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+
+
+
+
+        
