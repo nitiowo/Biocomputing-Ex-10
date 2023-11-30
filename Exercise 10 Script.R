d65d33d4db010c5a0c8e770035dfdef29eeec1fa
John LeSage <jlesage@nd.edu>
2023-11-29 21:21:00 -0500

    Working on question 1

--- Exercise 10 Script.R
+++ Exercise 10 Script.R
@@ -9,16 +9,27 @@ gameData <- read.table("UWvMSU_1-22-13.txt", header=TRUE, sep="\t")
 
 
 #Question 1
-gameFlow <- data.frame(Time=gameData$time, UW_Score<-0, MSU_Score<-0)
+#setting up data frames for question
+header <- c("Time", "Points")
+MSU_Points <- data.frame(matrix(ncol=2, nrow=nrow(gameData[gameData$team=="MSU",])))
+colnames(MSU_Points) <- header
+UW_Points <- data.frame(matrix(ncol=2, nrow=nrow(gameData[gameData$team=="UW",])))
+colnames(MSU_Points) <- header
+UW_Time <- 0
+UW_Score <- 0
+MSU_Time <- 0
+MSU_Score <- 0
+RowNumUW <- 0
+RowNumMSU <- 0
 #makes a data frame containing columns for time, UW score, and MSU score
 #this will be filled by our for loop:
 
-for(i in 1:5){
-  if(gameData$team[i]=="UW"){
-    gameFlow$UW_Score[i] <- gameFlow$UW_Score+gameData$score[i]
-  } else {
-    gameFlow$MSU_Score[i] <- gameFlow$MSU_Score+gameData$score[i]
-  }}
+for(i in 1:nrow(gameData)){
+  if(gameData$team[i] == "UW"){
+    UW_Score <- UW_Score + gameData$score[i]
+    UW_Time <- gameData$time[i]
+    RowNumUW <- 
+    
 
 
 #Question 2: Guess a number
@@ -26,13 +37,12 @@ myNum <- as.numeric(sample(100,1))
 for(i in 1:10){
   myGuess <- readline("Type your guess:")
   if(myGuess<myNum){
-    print("Too low, try again")
+    print("higher")
   } else if(myGuess>myNum){
-    print("Too high, try again")
+    print("lower")
   } else {
     print("That is correct!")
     break
   }}
-
 
 
