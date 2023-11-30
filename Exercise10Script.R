eaab8c7eeec233610b5ea67b677a91ae13401c93
kjackows <kjackows@nd.edu>
2023-11-30 15:09:34 -0500

    Question 1 Done

--- Exercise10Script.R
+++ Exercise10Script.R
@@ -1,6 +1,46 @@
 #Start in working directory that includes the file "UWvMSU_1-22-13.txt"
 #1)Generate graph of cumulative score over time for game data compared in the file
-##Tip: Generate dataframe with cumulative score for each team whenever either team scores
+##Tip: Generate data frame with cumulative score for each team whenever either team scores
+#read in data from score file
+ogscores<-read.csv(file="UWvMSU_1-22-13.txt", header=TRUE,sep="\t",stringsAsFactors=FALSE)
+#Separate data by team from original textfile
+UWscores<-data.frame(time=ogscores$time,score=ogscores$score,cumscore=0)
+for(i in 1:nrow(UWscores)){
+  UWscores$score[i]<-if(ogscores$team[i]=="UW"){
+    print(ogscores$score[i])
+  }else{
+    print(0)
+  }
+} 
+
+MSUscores<-data.frame(time=ogscores$time,score=ogscores$score,cumscore=0)
+for(i in 1:nrow(MSUscores)){
+  MSUscores$score[i]<-if(ogscores$team[i]=="MSU"){
+    print(ogscores$score[i])
+  }else{
+    print(0)
+  }
+} 
+#Fill cumulative column with cumulative score over time for each team
+for(i in 1:nrow(UWscores)){
+  UWscores$cumscore[i]=sum(UWscores$score[1:i])
+}
+
+for(i in 1:nrow(MSUscores)){
+  MSUscores$cumscore[i]=sum(MSUscores$score[1:i])
+}
+
+#Load in ggplot package to be able to creat plot
+library(ggplot2)
+
+#create lineplot with time on x axis and cumulative score on y axis
+ggplot(data=UWscores,
+       aes(time,cumscore)) + 
+  geom_line()+ 
+  xlab("Time") + 
+  ylab("Cumulative Score") +
+  geom_line(data=MSUscores,colour='darkorchid')
+
 #2)Write game called "guess my number" where computer generates number between 1 and 100 randomly, user types a number, and computer tells if it is higher or lower or correct and player can guess up to 10 times 
 #Create a vector containing numbers 1 through 100
 numbers<-c(1:100)
