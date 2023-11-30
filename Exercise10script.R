4a14d17422dff382ac9ff168c04de5eac12617a8
Conde <146041849+wmconde@users.noreply.github.com>
2023-11-30 17:45:58 -0500

    Part 1 and 2 Exercise10script.R

--- Exercise10script.R
+++ Exercise10script.R
@@ -24,3 +24,26 @@ for (i in 1:nrow(info)) {
 plot(info$time, UW-cum, type = "l", col = "red", ylim = c(0, max(UW_cum, MSU_cum)), xlab = "Time (min)", ylab = "Score", main = "Wisconsin vs MSU")
 lines(info$time, MSU_cum, col = "darkgreen")
 legend("bottomright", legend = c("University of Wisconsin", "MSU"), col = c("red", "darkgreen"), lty =1)
+
+#Part 2: Guess my number
+play <- function() {
+  randomnum <- sample(1:100, 1)
+  cat("I'm thinking of a number from 1-100...\n")
+  for (i in 1:10) {
+    user_guess <- as.numeric(readline(prompt = "Guess: "))
+    if (user_guess == randomnum) {
+      cat("Nice, You did it.\n")
+      break
+    } else {
+      if (user_guess < randomnum) {
+        cat("Higher\n")
+        } else {
+        cat("Lower\n")
+        }
+    }
+    if (i == 10) {
+      cat("You are very, very wrong. The correct number is ", randomnum, ".\n")
+    }
+  }
+}
+play()
